123ed77a3cfd79d0e1d2fcf7c02b73b299fc066d
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-04-05 19:23:09 -0700

    code updated

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -13,27 +13,36 @@ const Users = () => {
             .catch(error => {
                 console.error('Error fetching users:', error);
             });
-    }, [users]);
+    }, []);
+
+    const deleteUser = async (userId) => {
+        try {
+            await axios.delete(`https://server-alpha-taupe.vercel.app/users/${userId}`);
+            setUsers(users.filter(user => user._id !== userId));
+        } catch (error) {
+            console.error('Error deleting user:', error);
+        }
+    };
 
     return (
         <div className='container'>
             <div className='info'>
-            <h2>Users</h2>
-            <ul className='users'>
-                <div className='email'>
-                    <span>EMAIL</span>
-                    <span>PASSWORD</span>
-                </div>
-                {users.map(user => (
-                    <li key={user._id}>
-                        <span>{user.email}</span>
-                        <span>--{user.password}</span>
-                    </li>
-                
-                ))}
-            </ul>
+                <h2>Users</h2>
+                <ul className='users'>
+                    <div className='email'>
+                        <span>EMAIL</span>
+                        <span>PASSWORD</span>
+                        <span>DELETE USER</span>
+                    </div>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <span>{user.email}</span>
+                            <span>--{user.password}</span>
+                            <button onClick={() => deleteUser(user._id)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
-           
         </div>
     );
 };
